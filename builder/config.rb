1d244c9356d52cb78ea2fd2683ebbed3643c9198
urbanaux <urbanaux@users.noreply.github.com>
2016-12-11 12:21:16 +0100

    Mf95RF8qmv3APE

---
+++ builder/config.rb
@@ -0,0 +1,16 @@
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+set :haml, ugly: true, format: :html5
+set :build_dir, '../build'
+
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+end
